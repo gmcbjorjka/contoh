82dcd1ef28fbc0f9bcd0948995e4b2cc42c8027f
teguh <teguhgmc@gmail.com>
2022-11-14 08:50:41 +0700

    tes

---
+++ src/array.java
@@ -0,0 +1,21 @@
+public class array {
+    public static void main(String[] args) {
+//        String []nama=new String [4];
+//        int[]angka = new  int[3];
+//
+//        angka[0]=2;
+//        angka[2]=4;
+//        int s= angka[2];
+//
+//        nama[2]="budi";
+//        nama[4]="asih";
+//
+//        String u = nama[2];
+//        String t = nama [3];
+        int[]number = {1,45,7,34,89,12};
+        for (int index=0;index<number.length;index++){
+            System.out.println("angka dalam array number = "+ number[index]);
+        }
+    }
+
+}
